6cf0c851d5cb5f3695a96c04576b3c6eb39e29c4
Madeline Armstrong <armstrong.madeline@gmail.com>
2021-12-02 14:52:12 -0700

    built and tested cloths controller

--- app/controllers/api/cloths_controller.rb
+++ app/controllers/api/cloths_controller.rb
@@ -1,2 +1,49 @@
 class Api::ClothsController < ApplicationController
+  before_action :set_style
+  before_action :set_cloth, only: [:show, :update, :destroy]
+
+  def index
+    render json: @style.cloths
+  end
+
+  def show
+    render json: @cloth
+  end
+
+  def create
+    @cloth = @style.cloths.new(cloth_params)
+
+    if (@cloth.save)
+      render json: @cloth
+    else
+      render json: { errors: @cloth.errors }, status: 422
+    end
+  end
+
+  def update
+    if (@cloth.update(cloth_params))
+      render json: @cloth
+    else
+      render json: { errors: @cloth.errors }, status: 422
+    end
+  end
+
+  def destroy
+    render json: @cloth.destroy
+  end
+
+  private
+
+  def cloth_params
+    params.require(:cloth).permit(:item, :size, :color, :description)
+  end
+
+  def set_style
+    @style = Style.find(params[:style_id])
+  end
+
+  def set_cloth
+    @cloth = @style.cloths.find(params[:id])
+  end
+
 end
